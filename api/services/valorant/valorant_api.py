fa690d153b3c5079728a1dee9ad332218182fe39
geeeeorge <yuto.george@i.softbank.jp>
2022-06-12 11:16:27 +0900

    valorant_api触った

---
+++ api/services/valorant/valorant_api.py
@@ -0,0 +1,16 @@
+import os
+import valorant
+from dotenv import load_dotenv
+
+load_dotenv()
+KEY = os.environ["VALPY-KEY"]
+client = valorant.Client(KEY, locale=None)
+
+skins = client.get_skins()
+name = input("Search a Valorant Skin Collection: ")
+
+results = skins.find_all(name=lambda x: name.lower() in x.lower())
+
+print("\nResults: ")
+for skin in results:
+    print(f"\t{skin.name.ljust(21)} ({skin.localizedNames['ja-JP']})")
